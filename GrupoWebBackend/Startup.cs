ac42cf0a20a64fa36448f7b5ac2b2cbf98c3924e
Julio Barrios <85914860+Pandaluf@users.noreply.github.com>
2022-05-03 11:24:33 -0500

    fix.DomainAdoptionsRequest:remove files for errors
    Files that do not interact with the branch were removed due to bug problems. files are removed

--- GrupoWebBackend/Startup.cs
+++ GrupoWebBackend/Startup.cs
@@ -3,41 +3,6 @@ using GrupoWebBackend.DomainAdoptionsRequests.Domain.Repositories;
 using GrupoWebBackend.DomainAdoptionsRequests.Domain.Services;
 using GrupoWebBackend.DomainAdoptionsRequests.Persistence.Repositories;
 using GrupoWebBackend.DomainAdoptionsRequests.Services;
-using GrupoWebBackend.DomainAdvertisements.Domain.Repositories;
-using GrupoWebBackend.DomainAdvertisements.Domain.Services;
-using GrupoWebBackend.DomainAdvertisements.Persistence.Repositories;
-using GrupoWebBackend.DomainAdvertisements.Services;
-using GrupoWebBackend.DomainDistrict.Domain.Repositories;
-using GrupoWebBackend.DomainDistrict.Domain.Services;
-using GrupoWebBackend.DomainDistrict.Persistence.Repositories;
-using GrupoWebBackend.DomainDistrict.Services;
-using GrupoWebBackend.DomainPets.Domain.Repositories;
-using GrupoWebBackend.DomainPets.Domain.Services;
-using GrupoWebBackend.DomainPets.Persistence.Repositories;
-using GrupoWebBackend.DomainPets.Services;
-using GrupoWebBackend.DomainPublications.Domain.Repositories;
-using GrupoWebBackend.DomainPublications.Domain.Services;
-using GrupoWebBackend.DomainPublications.Persistence.Repositories;
-using GrupoWebBackend.DomainPublications.Services;
-using GrupoWebBackend.Security.Authorization.Handlers.Implementations;
-using GrupoWebBackend.Security.Authorization.Handlers.Interfaces;
-using GrupoWebBackend.Security.Authorization.Middleware;
-using GrupoWebBackend.Security.Authorization.Settings;
-using GrupoWebBackend.Security.Domain.Repositories;
-using GrupoWebBackend.Security.Domain.Services;
-using GrupoWebBackend.Security.Persistence.Repositories;
-using GrupoWebBackend.Security.Services;
-using GrupoWebBackend.Shared.Persistence.Context;
-using GrupoWebBackend.Shared.Persistence.Repositories;
-using Microsoft.AspNetCore.Builder;
-using Microsoft.AspNetCore.Hosting;
-using Microsoft.Extensions.Configuration;
-using Microsoft.Extensions.DependencyInjection;
-using Microsoft.Extensions.Hosting;
-using Microsoft.OpenApi.Models;
-using GrupoWebBackend.Shared.Domain.Repositories;
-using Microsoft.EntityFrameworkCore;
-
 
 namespace GrupoWebBackend
 {
@@ -77,20 +42,10 @@ namespace GrupoWebBackend
             services.AddDbContext<AppDbContext>();
             
             // Dependency Injection Configuration
-            services.AddScoped<IJwtHandler, JwtHandler>();
-            services.AddScoped<IUnitOfWork, UnitOfWork>();
-            services.AddScoped<IPetRepository, PetRepository>();
-            services.AddScoped<IPetService, PetService>();
-            services.AddScoped<IPublicationRepository, PublicationRepository>();
-            services.AddScoped<IPublicationService, PublicationService>();
+
             services.AddScoped<IAdoptionsRequestsRepository,AdoptionsRequestsRepository>();
             services.AddScoped<IAdoptionsRequestsService,AdoptionsRequestsService>();
-            services.AddScoped<IUserRepository, UserRepository>();
-            services.AddScoped<IUserService, UserService>();
-            services.AddScoped<IAdvertisementRepository, AdvertisementRepository>();
-            services.AddScoped<IAdvertisementService, AdvertisementService>();
-            services.AddScoped<IDistrictService, DistrictService>();
-            services.AddScoped<IDistrictRepository, DistrictRepository>();
+
             
             // AutoMapper Configuration
             services.AddAutoMapper(AppDomain.CurrentDomain.GetAssemblies());
